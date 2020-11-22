4504985d2d823a74b3c6f032891be671da575172
adha setiawan <adhasetiawanwiyana@gmail.com>
2020-11-22 17:44:34 +0800

    progress registrasi api

---
+++ middleware/auth.js
@@ -0,0 +1,49 @@
+var connection = require('../connection');
+var mysql = require('mysql');
+var md5 = require('md5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret');
+const conn = require('../connection');
+
+//registrasi
+exports.registrasi = function(req, res){
+    var post = {
+        id_user : req.body.id_user,
+        username : req.body.username,
+        email : req.body.email,
+        password: md5(req.body.password),
+        name : req.body.password,
+        address : req.body.address,
+        division : req.body.division,
+        role : req.body.role,
+        image : req.body.image
+    }
+
+    var query = "SELECT email FROM ?? WHERE ?? = ?";
+    var table = ["user_tbl", "email", post.email];
+
+    query = mysql.format(query,table);
+
+    connection.query(query, function(error, rows){
+        if (error){
+            console.log(error);
+        }else{
+            if(rows.length == 0 && req.file.filename){
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user_tbl"];
+
+                query = mysql.format(query,table);
+                connection.query(query, post, function(error, rows){
+                    if(error){
+                        console.log(error);
+                    }else{
+                        response.ok("Registration is successful", res);
+                    }
+                });
+            }else{
+                response.ok("Email already exist", res);
+            }
+        }
+    });
+};
