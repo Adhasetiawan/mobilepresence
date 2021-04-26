bb744ec88e49f4593ba13bf139d1f73eb79eeec3
adha setiawan <adhasetiawanwiyana@gmail.com>
2021-04-26 19:35:24 +0800

    Retrieve has been created

--- routes.js
+++ routes.js
@@ -2,7 +2,11 @@
 
 module.exports = function(app){
     var myjson = require('./controller');
+    var verifikasi = require ('./middleware/verifikasi')
 
     app.route('/')
     .get(myjson.index);
+
+    app.route('/trackrecord')
+    .get(verifikasi(), myjson.trackrec);
 }
