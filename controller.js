ac5d90b100687ff2151a1996075a429bbae2058c
adha setiawan <adhasetiawanwiyana@gmail.com>
2021-04-26 19:35:24 +0800

    Retrieve has been created

--- controller.js
+++ controller.js
@@ -3,7 +3,27 @@
 var response = require('./res');
 var connection = require('./connection');
 const conn = require('./connection');
+var mysql = require('mysql');
 
 exports.index = function (req, res){
     response.ok("Welcome", res);
+}
+
+exports.trackrec = function (req, res){
+    var search = {
+        id_user : req.body.id_user
+    };
+
+    var query = "SELECT * FROM trackrec WHERE ?? = ?"
+    var input = ["id_user", search.id_user];
+
+    query = mysql.format(query,input);
+    
+    connection.query(query, function(error,rows){
+        if(rows.length != 0){
+         res.json({success : true, data_track : rows});
+        }else{
+            console.log(error)
+        }
+    })
 }
